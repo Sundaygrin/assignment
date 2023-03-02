fac3fe18d65eb6f01438158f5760ee1dd5e6a759
sunday Charles <115741998+Sundaygrin@users.noreply.github.com>
2023-03-02 17:06:09 +0100

    Add files via upload

---
+++ ChristmassCarol.java
@@ -0,0 +1,19 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class ChristmassCarol {
+    public static void main(String[] args) {
+
+        int day;
+        for (day = 0; day <= 12; day++) {
+            System.out.println("On the Christmas" + day);
+        }
+        switch (day) {
+            case 1 :
+                System.out.println("");
+        }
+
+
+    }
+}
